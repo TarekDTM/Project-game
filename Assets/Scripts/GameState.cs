454b89fe6c6e916785cb71d5c2cc2502a6e83cb9
M.Aboelenein <m.aboelenein@theelephant.tech>
2020-03-22 20:49:16 +0200

    asdasd

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    [SerializeField] GameObject interactionCanvas;
+    [SerializeField] GameObject CrosshairCanvas;
+    bool somethingIsPickedUp = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionCanvas.SetActive(false);
+        CrosshairCanvas.SetActive(true);
+    }
+
+    public void DisableInteractionCanvas()
+    {
+        interactionCanvas.SetActive(false);
+    }
+    public void EnableInteractionCanvas()
+    {
+        interactionCanvas.SetActive(true);
+    }
+    public void DisableCrosshairCanvas()
+    {
+        CrosshairCanvas.SetActive(false);
+    }
+
+    public void EnableCrossHairCanvas()
+    {
+        CrosshairCanvas.SetActive(true);
+    }
+
+    public bool IsThereSomethingPickedUp()
+    {
+        return somethingIsPickedUp;
+    }
+    public void SomethingIsPickedUp()
+    {
+        somethingIsPickedUp = true;
+    }
+
+    public void NothingIsPickedUp()
+    {
+        somethingIsPickedUp = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    [SerializeField] float speed = 12f;
+    [SerializeField] float gravity = -9.8f;
+    [SerializeField] float jumpHeight = 3f;
+
+    public CharacterController controller;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundCheck();
+        MovePlayer();
+        Jump();
+        FreeFall();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+    
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+    }
+
+    private void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
+
+        controller.Move(movement * speed * Time.deltaTime);   
+    }
+    private void FreeFall()
+    {
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+   
+}
